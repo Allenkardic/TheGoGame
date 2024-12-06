eb59fce2775cee9869d162a16d105ee8b4972098
“aliakwe” <aliakwe@yahoo.com>
2024-12-06 01:44:36 -0500

    set up test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   preset: 'react-native',
+  preset: 'react-native',
+  // setupFiles: ['<rootDir>/jest/setup.js'],
+  setupFiles: ['./jest/setup.js'],
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|rollbar-react-native|@fortawesome|@react-native|@react-navigation|@react-native-async-storage/async-storage|react-native-flash-message|react-native/Libraries/Animated/NativeAnimatedHelper/*)',
+  ],
 };
